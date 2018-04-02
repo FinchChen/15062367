9fa563a59ca17e14c013d0264a2392b95af9ea67
yifanchen <yc16abc@herts.ac.uk>
2018-04-02 07:40:04 +0000

    Q1 solved

---
+++ Coursework/CourseworkTests/CourseworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  CourseworkTests.swift
+//  CourseworkTests
+//
+//  Created by yc16abc on 02/04/2018.
+//  Copyright © 2018 15062367. All rights reserved.
+//
+
+import XCTest
+@testable import Coursework
+
+class CourseworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
